8bf4587f157307dc9e77d0b43025a4a1069e61a7
Ian Siebold <iansiebold@gmail.com>
2019-09-19 18:11:53 -0500

    Updates

---
+++ precache-manifest.6d3cf97e57374242ebf3464123198ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e51bfc21d11eba422fa35fc7bc361b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d2dbcde22e633011007c",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36212995a8b017648ed4",
+    "url": "/my-app/static/js/2.cd930777.chunk.js"
+  },
+  {
+    "revision": "d2dbcde22e633011007c",
+    "url": "/my-app/static/js/main.3dfbf776.chunk.js"
+  },
+  {
+    "revision": "6811fe3071ccd3e79136",
+    "url": "/my-app/static/js/runtime-main.eed5ce82.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
